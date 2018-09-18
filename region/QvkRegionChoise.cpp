ea29f8ce64d899a3eaa257b913ebd89dee54ca57
Volker Kohaupt <vkohaupt@freenet.de>
2018-09-18 12:20:30 +0200

    Area and camera schow in windowbar

--- region/QvkRegionChoise.cpp
+++ region/QvkRegionChoise.cpp
@@ -58,7 +58,7 @@ QvkRegionChoise::QvkRegionChoise():handlePressed(NoHandle),
 #endif
 
     // Hint: Qt::WindowStaysOnTopHint is only for X11 and Windows on WayLand not do it
-    setWindowFlags( Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint | Qt::Tool );
+    setWindowFlags( Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint );
     setAttribute( Qt::WA_TranslucentBackground, true);
     setWindowState( Qt::WindowFullScreen );
 
