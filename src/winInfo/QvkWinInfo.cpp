d0c2795c9b5e89e8402015506b855406e22c986a
Volker Kohaupt <vkohaupt@freenet.de>
2018-10-19 14:30:01 +0200

    select window id

--- src/winInfo/QvkWinInfo.cpp
+++ src/winInfo/QvkWinInfo.cpp
@@ -1,12 +1,12 @@
 #include "QvkWinInfo.h"
 
 #ifdef Q_OS_LINUX
-   #include <QX11Info>
-   #include <X11/Xutil.h>
+#include <QX11Info>
+#include <X11/Xutil.h>
 #endif
 
 #ifdef Q_OS_WIN
-   #include <windows.h>
+#include <windows.h>
 #endif
 
 
@@ -17,66 +17,66 @@ QvkWinInfo::QvkWinInfo()
 
 void QvkWinInfo::slot_start()
 {
-  setWindowFlags( Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint );
+    setWindowFlags( Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint );
 
-  resize ( 50, 50 );
-  
-  QRegion Fenster( 0, 
-                   0, 
-                   50,
-                   50,
-                   QRegion::Rectangle );
+    resize ( 50, 50 );
 
-  QRegion mouseHole( 21,
-		     21,
-		     8,
-		     8,
-		     QRegion::Rectangle );
+    QRegion Fenster( 0,
+                     0,
+                     50,
+                     50,
+                     QRegion::Rectangle );
 
-  QRegion Rechteck_LO( 0,
-		       0,
-		       23,
-		       23,
-                       QRegion::Rectangle );
-		       
-  QRegion Rechteck_RO( 27,
-		       0,
-		       23,
-		       23,
-                       QRegion::Rectangle );
-		       
-  QRegion Rechteck_LU( 0,
-		       27,
-		       23,
-		       23,
-                       QRegion::Rectangle );
-		       
-  QRegion Rechteck_RU( 27,
-		       27,
-		       23,
-		       23,
+    QRegion mouseHole( 21,
+                       21,
+                       8,
+                       8,
                        QRegion::Rectangle );
 
-  QRegion r1 = Fenster.QRegion::subtracted( mouseHole );
-  QRegion r2 = r1.QRegion::subtracted( Rechteck_LO );
-  QRegion r3 = r2.QRegion::subtracted( Rechteck_RO );
-  QRegion r4 = r3.QRegion::subtracted( Rechteck_LU );
-  QRegion r5 = r4.QRegion::subtracted( Rechteck_RU );
+    QRegion Rechteck_LO( 0,
+                         0,
+                         23,
+                         23,
+                         QRegion::Rectangle );
 
-  this->setMask( r5 );
+    QRegion Rechteck_RO( 27,
+                         0,
+                         23,
+                         23,
+                         QRegion::Rectangle );
 
-  lastWinID = this->winId();
-  
-  windowTimer = new QTimer( this );
-  connect( windowTimer, SIGNAL( timeout() ), this, SLOT( slot_selectWindow() ) );
-  windowTimer->start( 500 );
-  
-  mouseTimer = new QTimer( this );
-  connect( mouseTimer, SIGNAL( timeout() ), this, SLOT( slot_mousePosition() ) );
-  mouseTimer->start( 20 );
+    QRegion Rechteck_LU( 0,
+                         27,
+                         23,
+                         23,
+                         QRegion::Rectangle );
 
-  show();
-  emit signal_showCursor( true );
+    QRegion Rechteck_RU( 27,
+                         27,
+                         23,
+                         23,
+                         QRegion::Rectangle );
+
+    QRegion r1 = Fenster.QRegion::subtracted( mouseHole );
+    QRegion r2 = r1.QRegion::subtracted( Rechteck_LO );
+    QRegion r3 = r2.QRegion::subtracted( Rechteck_RO );
+    QRegion r4 = r3.QRegion::subtracted( Rechteck_LU );
+    QRegion r5 = r4.QRegion::subtracted( Rechteck_RU );
+
+    this->setMask( r5 );
+
+    lastWinID = this->winId();
+
+    windowTimer = new QTimer( this );
+    connect( windowTimer, SIGNAL( timeout() ), this, SLOT( slot_selectWindow() ) );
+    windowTimer->start( 500 );
+
+    mouseTimer = new QTimer( this );
+    connect( mouseTimer, SIGNAL( timeout() ), this, SLOT( slot_mousePosition() ) );
+    mouseTimer->start( 20 );
+
+    show();
+    emit signal_showCursor( true );
 }
 
 
@@ -87,28 +87,49 @@ QvkWinInfo::~QvkWinInfo()
 
 void QvkWinInfo::paintEvent( QPaintEvent *event ) 
 {
-  (void)event;
-  QPainter painter( this );
-  painter.setPen( QPen( Qt::blue, 4 ) );
-  painter.drawLine( 50/2, 0, 50/2, 50);
-  painter.drawLine( 0, 50/2, 50, 50/2);
+    (void)event;
+    QPainter painter( this );
+    painter.setPen( QPen( Qt::blue, 4 ) );
+    painter.drawLine( 50/2, 0, 50/2, 50);
+    painter.drawLine( 0, 50/2, 50, 50/2);
 }
 
 
 void QvkWinInfo::slot_mousePosition()
 {
-  QCursor cursor;
-  move( cursor.pos().x() - 25 , cursor.pos().y() - 25 );
+    QCursor cursor;
+    move( cursor.pos().x() - 25 , cursor.pos().y() - 25 );
 }
 
-
 #ifdef Q_OS_LINUX
+static WindowList qxt_getWindows(Atom prop)
+{
+    WindowList res;
+    Atom type = 0;
+    int format = 0;
+    uchar* data = 0;
+    ulong count, after;
+    Display* display = QX11Info::display();
+    Window window = QX11Info::appRootWindow();
+    if (XGetWindowProperty(display, window, prop, 0, 1024 * sizeof(Window) / 4, False, AnyPropertyType,
+                           &type, &format, &count, &after, &data) == Success)
+    {
+        Window* list = reinterpret_cast<Window*>(data);
+        for (uint i = 0; i < count; ++i)
+            res += list[i];
+        if (data)
+            XFree(data);
+    }
+    return res;
+}
+
 WId QvkWinInfo::activeWindow()
 {
-  Window id;
-  int dummy;
-  XGetInputFocus( QX11Info::display() , &id, &dummy);
-  return id;
+    static Atom net_active = 0;
+    if (!net_active)
+        net_active = XInternAtom(QX11Info::display(), "_NET_ACTIVE_WINDOW", True);
+
+    return qxt_getWindows(net_active).value(0);
 }
 #endif
 
@@ -116,32 +137,32 @@ WId QvkWinInfo::activeWindow()
 #ifdef Q_OS_WIN
 WId QvkWinInfo::activeWindow()
 {
-  return (WId)GetForegroundWindow();
+    return (WId)GetForegroundWindow();
 }
 #endif
 
 
 void QvkWinInfo::slot_selectWindow()
 {
-  newWinID = activeWindow();
+    newWinID = activeWindow();
 
-  if ( lastWinID != newWinID )
-  {
-    windowTimer->stop();
-    mouseTimer->stop();
+    if ( lastWinID != newWinID )
+    {
+        windowTimer->stop();
+        mouseTimer->stop();
 
-    // Cursor resize does not show in video in the first Frames
-    resize( 10, 10 );
-    
-    emit signal_windowChanged( true );
-    this->close();
-  }
+        // Cursor resize does not show in video in the first Frames
+        resize( 10, 10 );
+
+        emit signal_windowChanged( true );
+        this->close();
+    }
 }
 
 
 // Need for Screenshot
 WId QvkWinInfo::getWinID()
 {
-  return newWinID;
+    return newWinID;
 }
 
--- src/winInfo/QvkWinInfo.h
+++ src/winInfo/QvkWinInfo.h
@@ -5,42 +5,46 @@
 #include <QPainter>
 #include <QMainWindow>
 
+#ifdef Q_OS_LINUX
+typedef QList<WId> WindowList;
+#endif
+
 class QvkWinInfo: public QMainWindow
 { 
-Q_OBJECT
+    Q_OBJECT
 public:
-  QvkWinInfo();
-  virtual ~QvkWinInfo();
-  WId getWinID();
+    QvkWinInfo();
+    virtual ~QvkWinInfo();
+    WId getWinID();
 
 
 public slots:
-  void slot_start();
+    void slot_start();
 
-  
+
 private slots: 
-   void slot_selectWindow();
-   void slot_mousePosition();
+    void slot_selectWindow();
+    void slot_mousePosition();
 
-   
+
 signals:
-  void signal_windowChanged( bool value );
-  void signal_showCursor( bool value );
-  
-protected:  
-  
-  
-private:
-  WId activeWindow();
+    void signal_windowChanged( bool value );
+    void signal_showCursor( bool value );
 
-  QTimer *windowTimer;
-  QTimer *mouseTimer;
-  
-  WId lastWinID;
-  WId newWinID;
-  
-  void paintEvent(QPaintEvent *event);
-  
+protected:  
+
+
+private:
+    static WId activeWindow();
+
+    QTimer *windowTimer;
+    QTimer *mouseTimer;
+
+    WId lastWinID;
+    WId newWinID;
+
+    void paintEvent(QPaintEvent *event);
+
 };
 
 #endif
