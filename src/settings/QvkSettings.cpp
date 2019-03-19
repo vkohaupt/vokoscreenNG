b9affa4d66b30f8c45c3b05053f672cb420e93fc
Volker Kohaupt <vkohaupt@freenet.de>
2019-03-19 20:28:31 +0100

    Starts minimized

--- src/settings/QvkSettings.cpp
+++ src/settings/QvkSettings.cpp
@@ -77,6 +77,13 @@ void QvkSettings::readAll( Ui_formMainWindow *ui_mainwindow, QMainWindow *parent
             continue;
         }
 
+        if ( ( listCheckBox.at(i)->objectName() == "checkBoxStartsMinimized" ) and
+            ( settings.value( listCheckBox.at(i)->objectName(), false ).toBool() == true ) )
+        {
+            parent->showMinimized();
+            listCheckBox.at(i)->click();
+            continue;
+        }
 
         // We found a setting, then we want set or not.
         if ( settings.value( listCheckBox.at(i)->objectName(), false ).toBool() == true )
