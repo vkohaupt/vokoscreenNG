2208d7aa61f493791d2ee33ef9c30413d2c3dfa7
Volker Kohaupt <vkohaupt@freenet.de>
2019-04-13 05:54:36 +0200

    2.9.6-beta

--- src/global.cpp
+++ src/global.cpp
@@ -5,6 +5,6 @@ namespace global
 {
     QString name = "vokoscreenNG";
     QString nameOutput = "[" + name + "]";
-    QString version = "2.9.5 Beta";
+    QString version = "2.9.6 Beta";
     bool VK_showOnlyFallbackIcons = false;
 }
