c1e100c0b0ceccbec47a239cd2daf43e3623b341
Volker Kohaupt <vkohaupt@freenet.de>
2019-06-08 18:54:46 +0200

    vokoscreen-deploy.bat

--- Deployment/vokoscreen.bat
+++ Deployment/vokoscreen.bat
@@ -3,11 +3,7 @@ cd /d %~d0%~p0
 set currentdir=%cd%
 set GSTREAMER_1_0_ROOT_X86=%currentdir%
 set GST_PLUGIN_PATH=%currentdir%
-echo %Path%
+rem echo %Path%
 
-vokoscreen.exe
-
-
-rem set GSTREAMER_1_0_ROOT_X86=C:\gstreamer\1.0\x86\
-rem set GST_PLUGIN_PATH=C:\gstreamer\1.0\x86\lib\gstreamer-1.0
-rem set Path=%Path%;C:\gstreamer\1.0\x86\bin
+rem Mit einem vorangestelltem "start" wird keine Konsole geöffnet
+start vokoscreenNG.exe
