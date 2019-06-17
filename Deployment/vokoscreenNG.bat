1b940825cfdd2c2545a89f42d2090f46301ec7c0
Volker Kohaupt <vkohaupt@freenet.de>
2019-06-17 20:29:20 +0200

    Windows: vokoscreen.bat

--- Deployment/vokoscreenNG.bat
+++ Deployment/vokoscreenNG.bat
@@ -1,9 +1,7 @@
-cd /d %~d0%~p0
 
+rem
+cd /d %~d0%~p0
 set currentdir=%cd%
 set GSTREAMER_1_0_ROOT_X86=%currentdir%
 set GST_PLUGIN_PATH=%currentdir%
-rem echo %Path%
-
-rem Mit einem vorangestelltem "start" wird keine Konsole geöffnet
 start vokoscreenNG.exe
