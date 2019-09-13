24a7e64be5920553480491ad309623c685f0a5ac
Volker Kohaupt <vkohaupt@freenet.de>
2019-09-13 06:13:42 +0200

    Licence

--- src/global.cpp
+++ src/global.cpp
@@ -29,6 +29,6 @@ namespace global
     QString name = "vokoscreenNG";
     QString nameOutput = "[" + name + "]";
     QString version = "2.9.8 Beta";
-    bool VK_showOnlyFallbackIcons = false;
+    bool VK_showOnlyFallbackIcons = true;
     QLineEdit *lineEditAudioPlug;
 }
