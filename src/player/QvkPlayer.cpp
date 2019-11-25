25745c11639822c46e4010a449fb27dff6462b37
Volker Kohaupt <vkohaupt@freenet.de>
2019-11-25 22:24:48 +0100

    PixelAspectRatio

--- src/player/QvkPlayer.cpp
+++ src/player/QvkPlayer.cpp
@@ -478,25 +478,33 @@ void QvkPlayer::slot_setNewImage( QImage image )
                               static_cast<int>( ui->framePlayer->height()*screen->devicePixelRatio() ),
                               Qt::KeepAspectRatio,
                               Qt::SmoothTransformation );
-    } else {
-        // Example:
-        // Area:320x800 scale:1280*720
-        if ( ratioSize.width() < ratioSize.height() )
-        {
-            image = image.scaled( (int)( (qreal)ui->framePlayer->height() * ( (qreal)image.width() / (qreal)image.height() * ratio ) * screen->devicePixelRatio() ),
-                                  static_cast<int>( ui->framePlayer->height() * screen->devicePixelRatio() ),
-                                  Qt::IgnoreAspectRatio,
-                                  Qt::SmoothTransformation );
-        } else  {
-            // Example:
-            // Area:800x320 scale:1280*720
-            image = image.scaled( static_cast<int>( ui->framePlayer->width() * screen->devicePixelRatio() ),
-                                  (int)( (qreal)ui->framePlayer->width() * ( (qreal)image.height() / (qreal)image.width() / ratio ) * screen->devicePixelRatio() ),
-                                  Qt::IgnoreAspectRatio,
-                                  Qt::SmoothTransformation );
-        }
+        ui->labelPlayer->setPixmap( QPixmap::fromImage( image ) );
+        return;
     }
-    ui->labelPlayer->setPixmap( QPixmap::fromImage( image ) );
+
+    // Example:
+    // Area:320x800 scale:1280*720
+    if ( ( ratio != 1 ) and ( ratioSize.width() < ratioSize.height() ) )
+    {
+        image = image.scaled( (int)( (qreal)ui->framePlayer->height() * ( (qreal)image.width() / (qreal)image.height() * ratio ) * screen->devicePixelRatio() ),
+                              static_cast<int>( ui->framePlayer->height() * screen->devicePixelRatio() ),
+                              Qt::IgnoreAspectRatio,
+                              Qt::SmoothTransformation );
+        ui->labelPlayer->setPixmap( QPixmap::fromImage( image ) );
+        return;
+    }
+
+    // Example:
+    // Area:800x320 scale:1280*720
+    if ( ( ratio != 1 ) and ( ratioSize.width() > ratioSize.height() ) )
+    {
+        image = image.scaled( static_cast<int>( ui->framePlayer->width() * screen->devicePixelRatio() ),
+                              (int)( (qreal)ui->framePlayer->width() * ( (qreal)image.height() / (qreal)image.width() / ratio ) * screen->devicePixelRatio() ),
+                              Qt::IgnoreAspectRatio,
+                              Qt::SmoothTransformation );
+        ui->labelPlayer->setPixmap( QPixmap::fromImage( image ) );
+        return;
+    }
 }
 
 
