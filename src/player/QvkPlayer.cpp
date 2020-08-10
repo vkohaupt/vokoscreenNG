900ab2c785152eee937c5affc633320f92c6873d
Volker Kohaupt <vkohaupt@freenet.de>
2020-08-10 08:42:52 +0200

    Player new icon for start stop pause etc.

--- src/player/QvkPlayer.cpp
+++ src/player/QvkPlayer.cpp
@@ -86,11 +86,17 @@ QvkPlayer::QvkPlayer( QMainWindow *parent, Ui_formMainWindow *ui_mainwindow ) : 
     QIcon iconStop( QString::fromUtf8( ":/pictures/player/stop.png" ) );
     ui->pushButtonStop->setIcon( iconStop );
 
-    ui->toolButtonFrameBackward->setIcon( QIcon::fromTheme( "go-previous",  style()->standardIcon( QStyle::SP_MediaSeekBackward ) ) );
-    ui->toolButtonFrameForward->setIcon( QIcon::fromTheme( "go-next",       style()->standardIcon( QStyle::SP_MediaSeekForward ) ) );
-    ui->toolButtonOpenFile->setIcon( QIcon::fromTheme( "document-open",     style()->standardIcon( QStyle::SP_FileIcon ) ) );
+    QIcon iconGoPrevios( QString::fromUtf8( ":/pictures/player/go-previous.png" ) );
+    ui->toolButtonFrameBackward->setIcon( iconGoPrevios );
+
+    QIcon iconGoNext( QString::fromUtf8( ":/pictures/player/go-next.png" ) );
+    ui->toolButtonFrameForward->setIcon( iconGoNext );
+
+    QIcon iconDocumentOpen( QString::fromUtf8( ":/pictures/player/document-open.png" ) );
+    ui->toolButtonOpenFile->setIcon( iconDocumentOpen );
+
     ui->toolButtonMute->setIcon( QIcon::fromTheme( "audio-volume-high"    , style()->standardIcon( QStyle::SP_MediaVolume ) ) );
-    ui->toolButtonFullscreen->setIcon( QIcon::fromTheme( "view-fullscreen", QIcon( ":/pictures/player/fullscreen.png" ) ) );
+    ui->toolButtonFullscreen->setIcon( QIcon( ":/pictures/player/fullscreen.png" ) );
 
     mediaPlayer = new QMediaPlayer;
     sliderVolume->setValue( 70 );
@@ -635,7 +641,7 @@ void QvkPlayer::vk_showFullscreen()
     ui->framePlayer->setStyleSheet( "QFrame { background-color: black; }"  );
     ui->widgetMenueBar->setStyleSheet( "QWidget { background-color: lightgray; }" );
     ui->labelMovePicture->setStyleSheet( "QLabel { background-color: lightgray; }" );
-    ui->toolButtonFullscreen->setIcon( QIcon::fromTheme( "view-restore", QIcon( ":/pictures/player/restore.png" ) ) );
+    ui->toolButtonFullscreen->setIcon( QIcon( ":/pictures/player/restore.png" ) );
     ui->labelMovePicture->show();
     ui->widgetMenueBar->show();
     ui->labelPlayer->setFocus();
@@ -660,7 +666,7 @@ void QvkPlayer::vk_showNormal()
     ui->framePlayer->setStyleSheet( "QFrame { background-color: black; }"  );
     // An empty string resets the color
     ui->widgetMenueBar->setStyleSheet( "" );
-    ui->toolButtonFullscreen->setIcon( QIcon::fromTheme( "view-fullscreen", QIcon( ":/pictures/player/fullscreen.png" ) ) );
+    ui->toolButtonFullscreen->setIcon( QIcon( ":/pictures/player/fullscreen.png" ) );
     ui->labelMovePicture->hide();
     ui->widgetMenueBar->show();
     ui->labelPlayer->setFocus();
