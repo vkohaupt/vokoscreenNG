8ce3b126faf1fa59464ac21fca744b966e35330c
Volker Kohaupt <vkohaupt@freenet.de>
2020-10-21 15:36:03 +0200

    clean code

--- src/camera/QvkCameraController.h
+++ src/camera/QvkCameraController.h
@@ -38,46 +38,46 @@ class QvkCameraController : public QObject
     Q_OBJECT
 
 public:
-   QvkCameraController( Ui_formMainWindow *ui_surface );
-   virtual ~QvkCameraController();
+    QvkCameraController( Ui_formMainWindow *ui_surface );
+    virtual ~QvkCameraController();
 
-   QvkCameraWindow *cameraWindow;
-   void getAllDevices();
+    QvkCameraWindow *cameraWindow;
+    void getAllDevices();
 
-  
+
 public slots:
 
 
 private slots:
-   void slot_addedCamera( QString description, QString device );
-   void slot_removedCamera( QString device );
-   void slot_startCamera( bool value );
+    void slot_addedCamera( QString description, QString device );
+    void slot_removedCamera( QString device );
+    void slot_startCamera( bool value );
 
-   void slot_statusChanged( QCamera::Status status );
-   void slot_stateChanged( QCamera::State state );
-   void slot_error( QCamera::Error error );
+    void slot_statusChanged( QCamera::Status status );
+    void slot_stateChanged( QCamera::State state );
+    void slot_error( QCamera::Error error );
 
-   void slot_setNewImage( QImage image );
+    void slot_setNewImage( QImage image );
 
-   void slot_frameOnOff( bool value );
-   void slot_sliderMoved( int value );
+    void slot_frameOnOff( bool value );
+    void slot_sliderMoved( int value );
 
-   void slot_resolutionChanged();
+    void slot_resolutionChanged();
 
 
 private:
-  Ui_formMainWindow *ui_formMainWindow;
-  QCamera *camera = nullptr;
-  QvkCameraWatcher *cameraWatcher;
-  QvkVideoSurface *videoSurface;
-  QvkSpezialSlider *sliderCameraWindowSize;
+    Ui_formMainWindow *ui_formMainWindow;
+    QCamera *camera = nullptr;
+    QvkCameraWatcher *cameraWatcher;
+    QvkVideoSurface *videoSurface;
+    QvkSpezialSlider *sliderCameraWindowSize;
 
 
 protected:
 
 
 signals:
-  void signal_resolutionChanged( bool );
+
 
 };
 
