9673d7408cd4dfeb611acd0b6c6f6a071d7b1012
Volker Kohaupt <vkohaupt@freenet.de>
2021-02-22 23:51:25 +0100

    Show primary screen in log

--- src/screenManager/QvkScreenManager.cpp
+++ src/screenManager/QvkScreenManager.cpp
@@ -48,6 +48,10 @@ void QvkScreenManager::slot_screen_count_changed()
         emit signal_clear_widget();
         for ( int i = 0; i < screen.size(); i++ )
         {
+            if ( screen.at(i)->name() == QGuiApplication::primaryScreen()->name() )
+            {
+                qDebug().noquote() << global::nameOutput << "This screen is the primary screen: " << QGuiApplication::primaryScreen()->name();
+            }
             qDebug().noquote() << global::nameOutput << "Name from screen: " << screen.at(i)->name();
             qDebug().noquote() << global::nameOutput << "Screen available desktop width :" << QString::number( screen.at(i)->geometry().width() * screen.at(i)->devicePixelRatio() );
             qDebug().noquote() << global::nameOutput << "Screen available desktop height:" << QString::number( screen.at(i)->geometry().height() * screen.at(i)->devicePixelRatio() );
