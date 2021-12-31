e81f0a5921cf20b9ed2c979cf4117edaa98ade3c
Volker Kohaupt <vkohaupt@freenet.de>
2021-12-31 08:37:32 +0100

    Release 3.1.0

--- src/global.cpp
+++ src/global.cpp
@@ -30,10 +30,10 @@ namespace global
     int showclickCounter = 0;
 
 #ifdef Q_OS_LINUX
-    QString version = "3.1.0 beta 08";
+    QString version = "3.1.0";
 #endif
 
 #ifdef Q_OS_WIN
-    QString version = "3.1.0 beta 08";
+    QString version = "3.1.0";
 #endif
 }
