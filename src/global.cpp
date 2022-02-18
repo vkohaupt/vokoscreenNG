97f6eac101d29406670ac03d398a970b8a55ed59
Volker Kohaupt <vkohaupt@freenet.de>
2022-02-18 08:38:43 +0100

    Beta 04

--- src/global.cpp
+++ src/global.cpp
@@ -30,10 +30,10 @@ namespace global
     int showclickCounter = 0;
 
 #ifdef Q_OS_LINUX
-    QString version = "3.2.0-beta-03";
+    QString version = "3.2.0-beta-04";
 #endif
 
 #ifdef Q_OS_WIN
-    QString version = "3.2.0-beta-03";
+    QString version = "3.2.0-beta-04";
 #endif
 }
