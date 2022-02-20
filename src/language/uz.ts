d3b2e62c3d8c4d55f79ed55f994b8667c332e3b9
umidjonalmasov <58512900+umidjonalmasov@users.noreply.github.com>
2022-02-20 11:13:42 +0500

    Add Uzbek translation

---
+++ src/language/uz.ts
@@ -0,0 +1,571 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uz">
+<context>
+    <name>QvkAudioController</name>
+    <message>
+        <source>Select one or more devices</source>
+        <translation>Bir yoki bir nechta qurilmani tanlang</translation>
+    </message>
+</context>
+<context>
+    <name>QvkCameraController</name>
+    <message>
+        <source>Camera</source>
+        <translation>Kamera</translation>
+    </message>
+</context>
+<context>
+    <name>QvkCameraWindow</name>
+    <message>
+        <source>Switch to Window</source>
+        <translation>Oynaga o&apos;tish</translation>
+    </message>
+    <message>
+        <source>Switch to Fullscreen</source>
+        <translation>Toʻliq ekran rejimiga oʻtish</translation>
+    </message>
+</context>
+<context>
+    <name>QvkCiscoFinishDialog</name>
+    <message>
+        <source>The setup was successful.</source>
+        <translation>Sozlash muvaffaqiyatli boʻldi.</translation>
+    </message>
+    <message>
+        <source>Close this window and start vokoscreenNG again.</source>
+        <translation>Ushbu oynani yoping va vokoscreenNG ni qayta ishga tushiring.</translation>
+    </message>
+    <message>
+        <source>Have fun with vokoscreenNG</source>
+        <translation>VokoscreenNG bilan zavqlaning</translation>
+    </message>
+</context>
+<context>
+    <name>QvkDirectSoundController</name>
+    <message>
+        <source>Select one or more devices</source>
+        <translation>Bir yoki bir nechta qurilmani tanlang</translation>
+    </message>
+</context>
+<context>
+    <name>QvkHelp</name>
+    <message>
+        <source>Help</source>
+        <translation>Yordam</translation>
+    </message>
+</context>
+<context>
+    <name>QvkImageFromTabs</name>
+    <message>
+        <source>No filemanager found.</source>
+        <translation>Hech qanday fayl boshqaruvchisi topilmadi.</translation>
+    </message>
+    <message>
+        <source>Please install a filemanager.</source>
+        <translation>Iltimos, fayl boshqaruvchisini o&apos;rnating.</translation>
+    </message>
+</context>
+<context>
+    <name>QvkLimitDiskFreeSpace</name>
+    <message>
+        <source>Warning</source>
+        <translation>Ogohlantirish</translation>
+    </message>
+    <message>
+        <source>The recording is not started, not enough free disk space.</source>
+        <translation>Запись не началась, недостаточно свободного места на диске.</translation>
+    </message>
+    <message>
+        <source>The free disk space limit is reached.</source>
+        <translation>Достигнут предел свободного дискового пространства.</translation>
+    </message>
+    <message>
+        <source>Limit of free disk space</source>
+        <translation>Предел свободного дискового пространства</translation>
+    </message>
+    <message>
+        <source>Megabyte</source>
+        <translation>Мегабайт</translation>
+    </message>
+    <message>
+        <source>The recording was stopped, not enough free disk space.</source>
+        <translation>Запись остановлена, недостаточно свободного места на диске.</translation>
+    </message>
+</context>
+<context>
+    <name>QvkMainWindow</name>
+    <message>
+        <source>Fullscreen</source>
+        <translation>To&apos;liq ekran</translation>
+    </message>
+    <message>
+        <source>Window</source>
+        <translation>Oyna</translation>
+    </message>
+    <message>
+        <source>No filemanager found.</source>
+        <translation>Hech qanday fayl boshqaruvchisi topilmadi.</translation>
+    </message>
+    <message>
+        <source>Please install a filemanager.</source>
+        <translation>Iltimos, fayl boshqaruvchisini o&apos;rnating.</translation>
+    </message>
+    <message>
+        <source>Sourcecode</source>
+        <translation>Manba kodi</translation>
+    </message>
+    <message>
+        <source>Homepage</source>
+        <translation>Uy sahifasi</translation>
+    </message>
+    <message>
+        <source>Translations</source>
+        <translation>Tarjimalar</translation>
+    </message>
+    <message>
+        <source>Donate</source>
+        <translation>Xayriya qilish</translation>
+    </message>
+</context>
+<context>
+    <name>QvkPulseAudioWatcher</name>
+    <message>
+        <source>Select one or more devices</source>
+        <translation>Bir yoki bir nechta qurilmani tanlang</translation>
+    </message>
+</context>
+<context>
+    <name>QvkRegionChoise</name>
+    <message>
+        <source>Area</source>
+        <translation>Maydon</translation>
+    </message>
+</context>
+<context>
+    <name>QvkSystray</name>
+    <message>
+        <source>Start</source>
+        <translation>Boshlash</translation>
+    </message>
+    <message>
+        <source>Stop</source>
+        <translation>To&apos;xtatish</translation>
+    </message>
+    <message>
+        <source>Pause</source>
+        <translation>Toʻxtatib turish</translation>
+    </message>
+    <message>
+        <source>Continue</source>
+        <translation>Davom etish</translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation>Chiqish</translation>
+    </message>
+    <message>
+        <source>Camera</source>
+        <translation>Kamera</translation>
+    </message>
+    <message>
+        <source>Magnification</source>
+        <translation>Лупа</translation>
+    </message>
+</context>
+<context>
+    <name>QvkSystrayAlternative</name>
+    <message>
+        <source>Start</source>
+        <translation>Boshlash</translation>
+    </message>
+    <message>
+        <source>Stop</source>
+        <translation>To&apos;xtatish</translation>
+    </message>
+    <message>
+        <source>Pause</source>
+        <translation>Toʻxtatib turish</translation>
+    </message>
+    <message>
+        <source>Continue</source>
+        <translation>Davom etish</translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation>Chiqish</translation>
+    </message>
+</context>
+<context>
+    <name>cameraSettingsDialog</name>
+    <message>
+        <source>Dialog</source>
+        <translation>Muloqot oynasi</translation>
+    </message>
+    <message>
+        <source>Flip horizontal</source>
+        <translation>Gorizontal aylantirish</translation>
+    </message>
+    <message>
+        <source>Flip vertical</source>
+        <translation>Vertikal aylantirish</translation>
+    </message>
+    <message>
+        <source>Invert</source>
+        <translation>Teskari aylantirish</translation>
+    </message>
+    <message>
+        <source>Gray</source>
+        <translation>Kulrang</translation>
+    </message>
+    <message>
+        <source>Black-and-white</source>
+        <translation>Qora va oq</translation>
+    </message>
+    <message>
+        <source>Remove window frame</source>
+        <translation>Oyna ramkasini olib tashlash</translation>
+    </message>
+    <message>
+        <source>160x120</source>
+        <translation>160x120</translation>
+    </message>
+    <message>
+        <source>320x240</source>
+        <translation>320x240</translation>
+    </message>
+    <message>
+        <source>Camera settings</source>
+        <translation>Kamera sozlamalari</translation>
+    </message>
+    <message>
+        <source>640x480</source>
+        <translation>640x480</translation>
+    </message>
+</context>
+<context>
+    <name>formMainWindow</name>
+    <message>
+        <source>Area</source>
+        <translation>Maydon</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>Qayta o&apos;rnatish</translation>
+    </message>
+    <message>
+        <source>Magnification</source>
+        <translation>Kattalashtirish</translation>
+    </message>
+    <message>
+        <source>Countdown</source>
+        <translation>Ortga hisoblash</translation>
+    </message>
+    <message>
+        <source>Video size:</source>
+        <translation>Video o&apos;lchami:</translation>
+    </message>
+    <message>
+        <source>Free disk space:</source>
+        <translation>Bo&apos;sh disk maydoni:</translation>
+    </message>
+    <message>
+        <source>Information</source>
+        <translation>Ma&apos;lumot</translation>
+    </message>
+    <message>
+        <source>Record Time:</source>
+        <translation>Yozib olish vaqti:</translation>
+    </message>
+    <message>
+        <source>Audio</source>
+        <translation>Audio</translation>
+    </message>
+    <message>
+        <source>Frames</source>
+        <translation>Kadrlar</translation>
+    </message>
+    <message>
+        <source>Videocodec</source>
+        <translation>Videokodek</translation>
+    </message>
+    <message>
+        <source>Format</source>
+        <translation>Format</translation>
+    </message>
+    <message>
+        <source>Audiocodec</source>
+        <translation>Audiokodek</translation>
+    </message>
+    <message>
+        <source>Quality</source>
+        <translation>Sifat</translation>
+    </message>
+    <message>
+        <source>Do not record mouse cursor</source>
+        <translation>Sichqoncha kursori yozilmasin</translation>
+    </message>
+    <message>
+        <source>Hour</source>
+        <translation>Soat</translation>
+    </message>
+    <message>
+        <source>Minute</source>
+        <translation>Daqiqa</translation>
+    </message>
+    <message>
+        <source>Start the timer</source>
+        <translation>Taymerni ishga tushirish</translation>
+    </message>
+    <message>
+        <source>Stop recording after</source>
+        <translation>Yozishni to&apos;xtatish</translation>
+    </message>
+    <message>
+        <source>Hours</source>
+        <translation>Soat</translation>
+    </message>
+    <message>
+        <source>Minutes</source>
+        <translation>Daqiqa</translation>
+    </message>
+    <message>
+        <source>Seconds</source>
+        <translation>Soniya</translation>
+    </message>
+    <message>
+        <source>Videopath</source>
+        <translation>Video yo‘li</translation>
+    </message>
+    <message>
+        <source>Limit of free disk space</source>
+        <translation>Diskdagi bo&apos;sh joy cheklovi</translation>
+    </message>
+    <message>
+        <source>Show in systray</source>
+        <translation>Treyda ko&apos;rsatish</translation>
+    </message>
+    <message>
+        <source>Second wait before recording</source>
+        <translation>Yozishdan oldin soniya kutish</translation>
+    </message>
+    <message>
+        <source>Look for updates</source>
+        <translation>Yangilanishlarni qidirish</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Yordam</translation>
+    </message>
+    <message>
+        <source>Video</source>
+        <translation>Video</translation>
+    </message>
+    <message>
+        <source>Available</source>
+        <translation>Mavjud</translation>
+    </message>
+    <message>
+        <source>Not available</source>
+        <translation>Mavjud emas</translation>
+    </message>
+    <message>
+        <source>Start</source>
+        <translation>Boshlash</translation>
+    </message>
+    <message>
+        <source>Stop</source>
+        <translation>To&apos;xtatish</translation>
+    </message>
+    <message>
+        <source>Pause</source>
+        <translation>Toʻxtatib turish</translation>
+    </message>
+    <message>
+        <source>Continue</source>
+        <translation>Davom etish</translation>
+    </message>
+    <message>
+        <source>Play</source>
+        <translation>Ijro ettirish</translation>
+    </message>
+    <message>
+        <source>Folder</source>
+        <translation>Jild</translation>
+    </message>
+    <message>
+        <source>Camera</source>
+        <translation>Kamera</translation>
+    </message>
+    <message>
+        <source>Invert</source>
+        <translation>Teskari aylantirish</translation>
+    </message>
+    <message>
+        <source>Gray</source>
+        <translation>Kulrang</translation>
+    </message>
+    <message>
+        <source>Send Report</source>
+        <translation>Hisobot yuborish</translation>
+    </message>
+    <message>
+        <source>CTRL+SHIFT+F10</source>
+        <translation>CTRL+SHIFT+F10</translation>
+    </message>
+    <message>
+        <source>CTRL+SHIFT+F11</source>
+        <translation>CTRL+SHIFT+F11</translation>
+    </message>
+    <message>
+        <source>CTRL+SHIFT+F12</source>
+        <translation>CTRL+SHIFT+F12</translation>
+    </message>
+    <message>
+        <source>Play last record</source>
+        <translation>Oxirgi yozuvni ijro ettirish</translation>
+    </message>
+    <message>
+        <source>Open folder</source>
+        <translation>Jildni ochish</translation>
+    </message>
+    <message>
+        <source>CTRL+SHIFT+F8</source>
+        <translation>CTRL+SHIFT+F8</translation>
+    </message>
+    <message>
+        <source>Minimized when recording starts</source>
+        <translation>Yozish boshlanganda minimallashtirish</translation>
+    </message>
+    <message>
+        <source>Starts minimized</source>
+        <translation>Minimallashtirilgan holda boshlanadi</translation>
+    </message>
+    <message>
+        <source>Flip horizontal</source>
+        <translation>Gorizontal aylantirish</translation>
+    </message>
+    <message>
+        <source>Flip vertical</source>
+        <translation>Vertikal aylantirish</translation>
+    </message>
+    <message>
+        <source>Black-and-white</source>
+        <translation>Qora va oq</translation>
+    </message>
+    <message>
+        <source>Remove window frame</source>
+        <translation>Oyna ramkasini olib tashlash</translation>
+    </message>
+    <message>
+        <source>Donate</source>
+        <translation>Xayriya qilish</translation>
+    </message>
+    <message>
+        <source>Reset all settings at the next start</source>
+        <translation>Keyingi ishga tushirishda barcha sozlamalarni tiklash</translation>
+    </message>
+    <message>
+        <source>Copyright and license from pictures</source>
+        <translation>Rasmlardan mualliflik huquqi va litsenziya</translation>
+    </message>
+    <message>
+        <source>Megabyte</source>
+        <translation>Megabayt</translation>
+    </message>
+    <message>
+        <source>Screencast</source>
+        <translation>Ekran yozuvi</translation>
+    </message>
+    <message>
+        <source>Player</source>
+        <translation>Pleyer</translation>
+    </message>
+    <message>
+        <source>Log</source>
+        <translation>Jurnal</translation>
+    </message>
+    <message>
+        <source>Automatically</source>
+        <translation>Avtomatik ravishda</translation>
+    </message>
+    <message>
+        <source>Create images of tabs </source>
+        <translation>Yorliqlar tasvirlarini yarating </translation>
+    </message>
+    <message>
+        <source>License information</source>
+        <translation>Litsenziya haqida ma&apos;lumot</translation>
+    </message>
+    <message>
+        <source>ON</source>
+        <translation>Yoqish</translation>
+    </message>
+    <message>
+        <source>OFF</source>
+        <translation>O&apos;chirish</translation>
+    </message>
+    <message>
+        <source>Global keyboard shortcuts</source>
+        <translation>Global klaviatura yorliqlari</translation>
+    </message>
+    <message>
+        <source>Assigned shortcut keys.</source>
+        <translation>Belgilangan yorliq tugmalari.</translation>
+    </message>
+    <message>
+        <source>Keyboard shortcut not assigned or assigned more than once.</source>
+        <translation>Klaviatura yorlig&apos;i tayinlanmagan yoki bir necha marta tayinlangan.</translation>
+    </message>
+    <message>
+        <source>If it shows assigned but does not work, it is being used by another program.</source>
+        <translation>Agar u tayinlanganligini ko&apos;rsatsa, lekin ishlamasa, u boshqa dastur tomonidan foydalanilmoqda.</translation>
+    </message>
+</context>
+<context>
+    <name>license</name>
+    <message>
+        <source>Copyright and license from pictures</source>
+        <translation>Rasmlardan mualliflik huquqi va litsenziya</translation>
+    </message>
+</context>
+<context>
+    <name>player</name>
+    <message>
+        <source>Play</source>
+        <translation>Ijro ettirish</translation>
+    </message>
+    <message>
+        <source>Pause</source>
+        <translation>Toʻxtatib turish</translation>
+    </message>
+    <message>
+        <source>Stop</source>
+        <translation>Toʻxtatish</translation>
+    </message>
+    <message>
+        <source>Open File</source>
+        <translation>Faylni ochish</translation>
+    </message>
+    <message>
+        <source>Fullscreen</source>
+        <translation>To&apos;liq ekran</translation>
+    </message>
+    <message>
+        <source>Previous frame</source>
+        <translation>Oldingi kadr</translation>
+    </message>
+    <message>
+        <source>Next frame</source>
+        <translation>Keyingi kadr</translation>
+    </message>
+    <message>
+        <source>Mute</source>
+        <translation>Ovozni o‘chirish</translation>
+    </message>
+    <message>
+        <source>Move menu</source>
+        <translation>Menyuni siljitish</translation>
+    </message>
+</context>
+</TS>
