e6dd755d2e90566c3d5df284c4b9601d0cabfade
Volker Kohaupt <vkohaupt@freenet.de>
2022-09-02 20:00:35 +0200

    Beta 05

--- src/global.cpp
+++ src/global.cpp
@@ -32,10 +32,10 @@ namespace global
     QLineEdit *lineEdit_EOS;
 
 #ifdef Q_OS_LINUX
-    QString version = "3.4.0-beta-04";
+    QString version = "3.4.0-beta-05";
 #endif
 
 #ifdef Q_OS_WIN
-    QString version = "3.4.0-beta-04";
+    QString version = "3.4.0-beta-05";
 #endif
 }
