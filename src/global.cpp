1f883c7dd408ee8ebfd4a2b548372bff94ce8dd7
Volker Kohaupt <vkohaupt@freenet.de>
2022-09-26 01:54:25 +0200

    Release 3.4.0

--- src/global.cpp
+++ src/global.cpp
@@ -32,10 +32,10 @@ namespace global
     QLineEdit *lineEdit_EOS;
 
 #ifdef Q_OS_LINUX
-    QString version = "3.4.0-beta-06";
+    QString version = "3.4.0";
 #endif
 
 #ifdef Q_OS_WIN
-    QString version = "3.4.0-beta-06";
+    QString version = "3.4.0";
 #endif
 }
