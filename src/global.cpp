bae786ba364c78e687b8663579f72d69594e3afb
Volker Kohaupt <vkohaupt@freenet.de>
2023-05-21 11:33:56 +0200

    3.7.0-beta-02

--- src/global.cpp
+++ src/global.cpp
@@ -33,10 +33,10 @@ namespace global
     bool testWASAPI = false;
 
 #ifdef Q_OS_LINUX
-    QString version = "3.7.0-beta-01";
+    QString version = "3.7.0-beta-02";
 #endif
 
 #ifdef Q_OS_WIN
-    QString version = "3.7.0-beta-01";
+    QString version = "3.7.0-beta-02";
 #endif
 }
