af9004ae460e15198dfbcbd37adb717709c34daf
Volker Kohaupt <vkohaupt@volkohde>
2023-12-09 04:32:06 +0100

    Wayland: Area

--- src/wayland/mainWindow_wl.cpp
+++ src/wayland/mainWindow_wl.cpp
@@ -107,7 +107,7 @@ QvkMainWindow_wl::QvkMainWindow_wl( QWidget *parent, Qt::WindowFlags f )
                 qDebug().noquote() << global::nameOutput << "Name from screen: " << screen.at(i)->name();
             }
             qDebug().noquote() << global::nameOutput << "myDevicePixelRatio:" << vkRegionChoise_wl->myDevicePixelRatio( screen.at(i) );
-            qDebug().noquote() << global::nameOutput << "DevicePixelRatio:" << screen.at(i)->devicePixelRatio();
+            qDebug().noquote() << global::nameOutput << "QScreen::devicePixelRatio:" << screen.at(i)->devicePixelRatio();
             qDebug().noquote() << global::nameOutput << "Screen Resolution width :" << screen.at(i)->geometry().width();
             qDebug().noquote() << global::nameOutput << "Screen Resolution height :" << screen.at(i)->geometry().height();
             qDebug().noquote() << global::nameOutput << "Vertical refresh rate of the screen in Hz:" << screen.at(i)->refreshRate();
