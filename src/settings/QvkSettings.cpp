f1b01f9c6be11c3d0188dee623f829f2d0b736ed
Volker Kohaupt <vkohaupt@volkohde>
2024-04-28 07:11:06 +0200

    Systrayalternative

--- src/settings/QvkSettings.cpp
+++ src/settings/QvkSettings.cpp
@@ -647,7 +647,7 @@ void QvkSettings::readSystrayAlternative( QvkSystrayAlternative *vkSystrayAltern
 {
     QSettings settings( QSettings::IniFormat, QSettings::UserScope, global::name, global::name, Q_NULLPTR );
     settings.beginGroup( "SystrayAlternative" );
-    vkSystrayAlternative->vkSystrayAlternativeWindow->move( settings.value( "X", 0 ).toInt(), settings.value( "Y", 0 ).toInt() );
+    vkSystrayAlternative->vkSystrayAlternativeWindow->move( settings.value( "X", 30 ).toInt(), settings.value( "Y", 30 ).toInt() );
     settings.endGroup();
 }
 
