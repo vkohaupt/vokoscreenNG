737aabbb5fe650bf696228b69663eeffec24ad38
Volker Kohaupt <vkohaupt@volkoh.de>
2024-05-15 06:01:03 +0200

    Windows: wasap2

--- src/audio/windows/WASAPI/wasapi2-launch.bat
+++ src/audio/windows/WASAPI/wasapi2-launch.bat
@@ -8,7 +8,7 @@ C:\gstreamer\1.0\msvc_x86_64\bin\gst-launch-1.0.exe -e ^
         ! h264parse ^
         ! queue ^
         ! mux. ^
-    wasapisrc low-latency=true role=multimedia device="\\\\\?\\SWD\#MMDEVAPI\#\{0.0.1.00000000\}.\{2dd319ce-b622-4b87-b789-cc74e2b0d457\}\#\{2eef81be-33fa-4800-9670-1cd474972c3f\}" ^
+    wasapi2src low-latency=true device="\\\\\?\\SWD\#MMDEVAPI\#\{0.0.1.00000000\}.\{2dd319ce-b622-4b87-b789-cc74e2b0d457\}\#\{2eef81be-33fa-4800-9670-1cd474972c3f\}" ^
         ! audioconvert ^
         ! audiorate ^
         ! queue max-size-bytes=1000000 max-size-time=10000000000 max-size-buffers=1000 ^
