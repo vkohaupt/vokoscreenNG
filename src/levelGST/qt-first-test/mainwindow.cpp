0cc895a367807dd9424fc9661e7d17e220e0e3ae
Volker Kohaupt <vkohaupt@freenet.de>
2024-05-26 14:19:27 +0200

    gst level

--- src/levelGST/qt-first-test/mainwindow.cpp
+++ src/levelGST/qt-first-test/mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include "ui_formMainWindow.h"
+#include "QvkAudioController.h"
 
 QvkMainWindow::QvkMainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::formMainWindow)
 {
     ui->setupUi(this);
+
+    QvkAudioController *vkAudioController = new QvkAudioController( ui );
+    vkAudioController->init();
 }
 
 QvkMainWindow::~QvkMainWindow()
