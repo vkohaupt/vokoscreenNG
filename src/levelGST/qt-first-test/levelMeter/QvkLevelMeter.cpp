5e395f9bd0c6cda4e109f6cd63026223f8bfc566
Volker Kohaupt <vkohaupt@freenet.de>
2024-05-27 23:57:56 +0200

    gst level

--- src/levelGST/qt-first-test/levelMeter/QvkLevelMeter.cpp
+++ src/levelGST/qt-first-test/levelMeter/QvkLevelMeter.cpp
@@ -42,41 +42,35 @@ QvkLevelMeter::~QvkLevelMeter()
 }
 
 
-static gboolean message_handler (GstBus * bus, GstMessage * message, gpointer data)
+static gboolean message_handler( GstBus * bus, GstMessage * message, gpointer data )
 {
     Q_UNUSED(bus)
 
-    if (message->type == GST_MESSAGE_ELEMENT) {
-        const GstStructure *s = gst_message_get_structure (message);
-        const gchar *name = gst_structure_get_name (s);
+    if ( message->type == GST_MESSAGE_ELEMENT ) {
+        const GstStructure *s = gst_message_get_structure( message );
+        const gchar *name = gst_structure_get_name( s );
 
-        if (strcmp (name, "level") == 0) {
+        if ( strcmp( name, "level" ) == 0 ) {
             gint channels;
-            GstClockTime endtime;
             gdouble rms_dB;
             gdouble rms;
             const GValue *array_val;
             const GValue *value;
             GValueArray *rms_arr;
 
-            if (!gst_structure_get_clock_time (s, "endtime", &endtime)) {
-                g_warning ("Could not parse endtime");
-            }
-
             // the values are packed into GValueArrays with the value per channel
-            array_val = gst_structure_get_value (s, "rms");
-            rms_arr = (GValueArray *) g_value_get_boxed (array_val);
+            array_val = gst_structure_get_value( s, "rms" );
+            rms_arr = (GValueArray *) g_value_get_boxed( array_val );
 
             // we can get the number of channels as the length of any of the value arrays
             channels = rms_arr->n_values;
 
-            for (gint i = 0; i < channels; ++i) {
-
-                value = g_value_array_get_nth (rms_arr, i);
-                rms_dB = g_value_get_double (value);
+            for ( gint i = 0; i < channels; ++i ) {
+                value = g_value_array_get_nth( rms_arr, i );
+                rms_dB = g_value_get_double( value );
 
                 // converting from dB to normal gives us a value between 0.0 and 1.0
-                rms = pow (10, rms_dB / 20);
+                rms = pow( 10, rms_dB / 20 );
 
                 qint64 index = (qint64)data;
                 if ( index == 0 ) {
@@ -105,42 +99,42 @@ void QvkLevelMeter::start( QString device, QString index )
     GstCaps *caps;
     GstBus *bus;
 
-    caps = gst_caps_from_string ("audio/x-raw,channels=2");
+    caps = gst_caps_from_string( "audio/x-raw,channels=2" );
 
-    pipeline = gst_pipeline_new (NULL);
+    pipeline = gst_pipeline_new( NULL );
     g_assert (pipeline);
-    audiotestsrc = gst_element_factory_make ("pulsesrc", NULL);
+    audiotestsrc = gst_element_factory_make( "pulsesrc", NULL );
     g_assert (audiotestsrc);
-    audioconvert = gst_element_factory_make ("audioconvert", NULL);
+    audioconvert = gst_element_factory_make( "audioconvert", NULL );
     g_assert (audioconvert);
-    level = gst_element_factory_make ("level", NULL);
+    level = gst_element_factory_make( "level", NULL );
     g_assert (level);
-    fakesink = gst_element_factory_make ("fakesink", NULL);
+    fakesink = gst_element_factory_make( "fakesink", NULL );
     g_assert (fakesink);
 
-    gst_bin_add_many (GST_BIN (pipeline), audiotestsrc, audioconvert, level, fakesink, NULL);
-    if (!gst_element_link (audiotestsrc, audioconvert)) {
-        g_error ("Failed to link audiotestsrc and audioconvert");
+    gst_bin_add_many( GST_BIN( pipeline ), audiotestsrc, audioconvert, level, fakesink, NULL );
+    if ( !gst_element_link( audiotestsrc, audioconvert ) ) {
+        g_error( "Failed to link audiotestsrc and audioconvert" );
     }
-    if (!gst_element_link_filtered (audioconvert, level, caps)) {
-        g_error ("Failed to link audioconvert and level");
+    if (!gst_element_link_filtered( audioconvert, level, caps ) ) {
+        g_error( "Failed to link audioconvert and level" );
     }
-    if (!gst_element_link (level, fakesink)) {
-        g_error ("Failed to link level and fakesink");
+    if ( !gst_element_link( level, fakesink ) ) {
+        g_error( "Failed to link level and fakesink" );
     }
 
     g_object_set( G_OBJECT( audiotestsrc ), "device", device.toUtf8().constData(), NULL );
 
     // make sure we'll get messages
-    g_object_set (G_OBJECT (level), "post-messages", TRUE, NULL);
+    g_object_set( G_OBJECT( level ), "post-messages", TRUE, NULL );
     // run synced and not as fast as we can
-    g_object_set (G_OBJECT (fakesink), "sync", TRUE, NULL);
+    g_object_set( G_OBJECT( fakesink ), "sync", TRUE, NULL );
 
     bus = gst_element_get_bus (pipeline);
 
     gint64 msg = index.toInt();
     gst_bus_set_sync_handler( bus, (GstBusSyncHandler)message_handler, (gpointer)msg, NULL );
 
-    gst_element_set_state (pipeline, GST_STATE_PLAYING);
+    gst_element_set_state( pipeline, GST_STATE_PLAYING );
 }
 
