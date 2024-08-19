7a6f635b95d301b9832e85af39bb8c85934d66f2
Volker Kohaupt <vkohaupt@freenet.de>
2024-08-19 16:12:30 +0200

    Profile

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -179,6 +179,7 @@ private:
 
     QString Profile;
     QString Profile_None = Profile.fill( '-', 20 );
+    void set_ToolButtonEnableDisable();
 
 
 private slots:
@@ -237,7 +238,6 @@ private slots:
     void slot_profileLoad(int index);
     void slot_profileNew(bool bo);
     void slot_profileDelete(bool bo);
-    void set_ToolButtonEnableDisable();
 
 
 signals:
