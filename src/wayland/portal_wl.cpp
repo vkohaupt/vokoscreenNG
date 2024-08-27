b2eafff3d74bc7ba8a729c806180365dddfc80db
Volker Kohaupt <vkohaupt@freenet.de>
2024-08-27 09:14:08 +0200

    Wayland: new portal

--- src/wayland/portal_wl.cpp
+++ src/wayland/portal_wl.cpp
@@ -19,8 +19,7 @@ const QDBusArgument &operator >> (const QDBusArgument &arg, Portal_wl::Stream &s
 
     arg.beginMap();
 
-    while (!arg.atEnd())
-    {
+    while ( !arg.atEnd() ) {
         QString key;
         QVariant value;
         arg.beginMapEntry();
@@ -35,27 +34,25 @@ const QDBusArgument &operator >> (const QDBusArgument &arg, Portal_wl::Stream &s
     return arg;
 }
 
-Portal_wl::Portal_wl(QObject* parent)
+Portal_wl::Portal_wl( QObject* parent )
     : QObject{parent}
 {
 }
 
 Portal_wl::~Portal_wl()
 {
-    if (mScreencastPortal)
-    {
+    if ( mScreencastPortal ) {
         delete mScreencastPortal;
     }
 }
 
-void Portal_wl::startScreenCast(bool withCursor)
+void Portal_wl::startScreenCast( bool withCursor )
 {
     mWithCursor = withCursor;
 
     QDBusInterface* portal = screencastPortal();
 
-    if (!portal)
-    {
+    if ( !portal ) {
         return;
     }
 
@@ -70,8 +67,7 @@ void Portal_wl::startScreenCast(bool withCursor)
 
     const QDBusReply<QDBusObjectPath> reply = portal->call("CreateSession", options);
 
-    if (!reply.isValid())
-    {
+    if ( !reply.isValid() ) {
         qWarning() << "Couldn't get reply to ScreenCast/CreateSession";
         qWarning() << "Error: " << reply.error().message();
         emit screenCastAborted();
@@ -81,19 +77,16 @@ void Portal_wl::startScreenCast(bool withCursor)
 
 void Portal_wl::stopScreenCast()
 {
-    if (mSession.isEmpty())
-    {
+    if ( mSession.isEmpty() ) {
         return;
     }
 
-    QDBusInterface portal("org.freedesktop.portal.Desktop", mSession, "org.freedesktop.portal.Session");
+    QDBusInterface portal( "org.freedesktop.portal.Desktop", mSession, "org.freedesktop.portal.Session" );
 
-    if (portal.isValid())
-    {
+    if ( portal.isValid() ) {
         const QDBusReply<void> reply = portal.call("Close");
 
-        if (!reply.isValid())
-        {
+        if ( !reply.isValid() ) {
             qWarning() << "Couldn't get reply to ScreenCast/Close";
             qWarning() << "Error: " << reply.error().message();
             mSession.clear();
@@ -102,10 +95,9 @@ void Portal_wl::stopScreenCast()
     }
 }
 
-void Portal_wl::handleCreateSessionResponse(uint response, const QVariantMap& results)
+void Portal_wl::handleCreateSessionResponse( uint response, const QVariantMap& results )
 {
-    if (response != 0)
-    {
+    if ( response != 0 ) {
         qWarning() << "Failed to create session: " << response << results;
         emit screenCastAborted();
         return;
@@ -115,8 +107,7 @@ void Portal_wl::handleCreateSessionResponse(uint response, const QVariantMap& re
 
     QDBusInterface* portal = screencastPortal();
 
-    if (!portal)
-    {
+    if ( !portal ) {
         return;
     }
 
@@ -129,8 +120,7 @@ void Portal_wl::handleCreateSessionResponse(uint response, const QVariantMap& re
     options["handle_token"] = requestToken;
     options["persist_mode"] = PERSISTENT;
 
-    if (!mRestoreToken.isEmpty())
-    {
+    if ( !mRestoreToken.isEmpty() ) {
         options["restore_token"] = mRestoreToken;
     }
 
@@ -140,8 +130,7 @@ void Portal_wl::handleCreateSessionResponse(uint response, const QVariantMap& re
 
     const QDBusReply<QDBusObjectPath> reply = portal->call("SelectSources", QDBusObjectPath(mSession), options);
 
-    if (!reply.isValid())
-    {
+    if ( !reply.isValid() ) {
         qWarning() << "Couldn't get reply";
         qWarning() << "Error: " << reply.error().message();
         emit screenCastAborted();
@@ -149,12 +138,11 @@ void Portal_wl::handleCreateSessionResponse(uint response, const QVariantMap& re
     }
 }
 
-void Portal_wl::handleSelectSourcesResponse(uint response, const QVariantMap& results)
+void Portal_wl::handleSelectSourcesResponse( uint response, const QVariantMap& results )
 {
     Q_UNUSED(results);
 
-    if (response != 0)
-    {
+    if ( response != 0 ) {
         qWarning() << "Failed to select sources: " << response;
         emit screenCastAborted();
         return;
@@ -162,8 +150,7 @@ void Portal_wl::handleSelectSourcesResponse(uint response, const QVariantMap& re
 
     QDBusInterface* portal = screencastPortal();
 
-    if (!portal)
-    {
+    if ( !portal ) {
         return;
     }
 
@@ -177,8 +164,7 @@ void Portal_wl::handleSelectSourcesResponse(uint response, const QVariantMap& re
 
     const QDBusReply<QDBusObjectPath> reply = portal->call("Start", QDBusObjectPath(mSession), "", options);
 
-    if (!reply.isValid())
-    {
+    if ( !reply.isValid() ) {
         qWarning() << "Couldn't get reply";
         qWarning() << "Error: " << reply.error().message();
         emit screenCastAborted();
@@ -186,12 +172,11 @@ void Portal_wl::handleSelectSourcesResponse(uint response, const QVariantMap& re
     }
 }
 
-void Portal_wl::handleStartResponse(uint response, const QVariantMap& results)
+void Portal_wl::handleStartResponse( uint response, const QVariantMap& results )
 {
     Q_UNUSED(results);
 
-    if (response != 0)
-    {
+    if ( response != 0 ) {
         // The system Desktop dialog was canceled
         qDebug() << "Failed to start or cancel dialog: " << response;
         emit screenCastAborted();
@@ -206,8 +191,7 @@ void Portal_wl::handleStartResponse(uint response, const QVariantMap& results)
 
     QDBusInterface* portal = screencastPortal();
 
-    if (!portal)
-    {
+    if ( !portal ) {
         return;
     }
 
@@ -215,8 +199,7 @@ void Portal_wl::handleStartResponse(uint response, const QVariantMap& results)
     QMap<QString, QVariant> options;
     const QDBusReply<QDBusUnixFileDescriptor> reply = portal->call("OpenPipeWireRemote", QDBusObjectPath(mSession), options);
 
-    if (!reply.isValid())
-    {
+    if ( !reply.isValid() ) {
         qWarning() << "Couldn't get reply";
         qWarning() << "Error: " << reply.error().message();
         emit screenCastAborted();
@@ -226,13 +209,12 @@ void Portal_wl::handleStartResponse(uint response, const QVariantMap& results)
     const quint64 fd = reply.value().fileDescriptor();
     const QString path = QString::number(stream.node_id);
 
-    emit streamStarted(fd, path);
+    emit streamStarted( fd, path );
 }
 
 QDBusInterface* Portal_wl::screencastPortal()
 {
-    if (!mScreencastPortal)
-    {
+    if (!mScreencastPortal) {
         mScreencastPortal = new QDBusInterface("org.freedesktop.portal.Desktop", "/org/freedesktop/portal/desktop",
                                                "org.freedesktop.portal.ScreenCast");
         mScreencastPortal->setParent(this);
@@ -241,8 +223,7 @@ QDBusInterface* Portal_wl::screencastPortal()
         qDebug() << "request path" << mRequestPath;
     }
 
-    if (mScreencastPortal->isValid())
-    {
+    if ( mScreencastPortal->isValid() ) {
         return mScreencastPortal;
     }
 
@@ -255,7 +236,8 @@ QString Portal_wl::createSessionToken() const
     static int sessionTokenCounter = 0;
 
     sessionTokenCounter += 1;
-    return QString("obsess%1").arg(sessionTokenCounter);
+    return QString("vosess%1").arg(sessionTokenCounter);
+
 }
 
 QString Portal_wl::createRequestToken() const
@@ -263,5 +245,5 @@ QString Portal_wl::createRequestToken() const
     static int requestTokenCounter = 0;
 
     requestTokenCounter += 1;
-    return QString("obreq%1").arg(requestTokenCounter);
+    return QString("voreq%1").arg(requestTokenCounter);
 }
