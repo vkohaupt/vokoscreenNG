ff02555633641cd6591b3aa925a7202d8ef889fd
Volker Kohaupt <vkohaupt@freenet.de>
2024-08-27 09:02:55 +0200

    Wayland: new portal

--- src/wayland/portal_wl.cpp
+++ src/wayland/portal_wl.cpp
@@ -1,39 +1,9 @@
-/*
- * Copyright (C) 2015-2024 Département de l'Instruction Publique (DIP-SEM)
- *
- * Copyright (C) 2013 Open Education Foundation
- *
- * Copyright (C) 2010-2013 Groupement d'Intérêt Public pour
- * l'Education Numérique en Afrique (GIP ENA)
- *
- * This file is part of OpenBoard.
- *
- * OpenBoard is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3 of the License,
- * with a specific linking exception for the OpenSSL project's
- * "OpenSSL" library (or with modified versions of it that use the
- * same license as the "OpenSSL" library).
- *
- * OpenBoard is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with OpenBoard. If not, see <http://www.gnu.org/licenses/>.
- */
-
-// #include "UBDesktopPortal.h"
 #include "portal_wl.h"
 
 #include <QDBusInterface>
 #include <QDBusReply>
 #include <QDBusUnixFileDescriptor>
 #include <QDebug>
-#include <QUrl>
-#include <QFile>
-#include <QPixmap>
 
 enum : uint { MONITOR = 1, WINDOW = 2, VIRTUAL = 4 } SourceType;
 enum : uint { HIDDEN = 1, EMBEDDED = 2, METADATA = 4 } CursorMode;
