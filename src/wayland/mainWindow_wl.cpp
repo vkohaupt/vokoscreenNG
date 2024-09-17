27534f32204760456c4dfbed5e6e11653388d832
Volker Kohaupt <vkohaupt@volkohde>
2024-09-17 09:11:54 +0200

    Wayland closeevent

--- src/wayland/mainWindow_wl.cpp
+++ src/wayland/mainWindow_wl.cpp
@@ -153,6 +153,7 @@ QvkMainWindow_wl::~QvkMainWindow_wl()
 void QvkMainWindow_wl::closeEvent( QCloseEvent *event )
 {
     Q_UNUSED(event);
+    ui->pushButtonStop->click();
     vkRegionChoise_wl->close();
     vkSettings.saveAll( ui, this );
 }
