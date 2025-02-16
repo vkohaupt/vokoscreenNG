a1a9cb8fdb116f43cc696c6a460600f9566c1df9
Volker Kohaupt <vkohaupt@freenet.de>
2025-02-16 06:27:28 +0100

    Release 4.4.5

--- src/global.cpp
+++ src/global.cpp
@@ -26,7 +26,7 @@
 
 namespace global
 {
-    QString version = "4.4.3";
+    QString version = "4.4.5";
     QString name = "vokoscreenNG";
     QString nameOutput = "[" + name + "]";
     int showclickCounter = 0;
