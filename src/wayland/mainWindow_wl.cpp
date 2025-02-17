29b428301135b861512b0bd15cf2e3313d481c90
Volker Kohaupt <vkohaupt@freenet.de>
2025-02-17 10:19:13 +0100

    Wayland: Add separated audio tracks reset

--- src/wayland/mainWindow_wl.cpp
+++ src/wayland/mainWindow_wl.cpp
@@ -107,6 +107,9 @@ QvkMainWindow_wl::QvkMainWindow_wl( QWidget *parent, Qt::WindowFlags f )
     QvkAudioController_wl *vkAudioController = new QvkAudioController_wl( ui );
     vkAudioController->init();
 
+    connect( ui->toolButtonSeparatedAudioTracksReset, &QToolButton::clicked, this, [=]() { ui->checkBoxSeparatedAudioTracks->setCheckState( Qt::Unchecked); } );
+
+
     vkHelp = new QvkHelp_wl( ui );
     new QvkLicenses( ui->pushButtonLicense );
     new QvkImageFromTabs_wl( this );
