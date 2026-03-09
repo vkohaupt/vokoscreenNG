aaab0fc3e1a7a35806b897005eab0963d6eea108
Volker Kohaupt <vkohaupt@freenet.de>
2026-03-09 14:30:46 +0100

    Flatpak

---
+++ info-licences-changelog-install/Flatpak/vokoscreenFlatpak-x264/make_vokoscreen_flatpak.sh
@@ -0,0 +1,17 @@
+#/usr/bin/sh
+
+rm -rf com.github.vkohaupt.vokoscreenNG
+
+#flatpak -y --user install org.kde.Sdk//5.15-22.08
+#flatpak -y --user install org.kde.Platform//5.15-22.08
+flatpak -y --user install org.kde.Sdk//6.9
+flatpak -y --user install org.kde.Platform//6.9
+
+git clone https://github.com/flathub/com.github.vkohaupt.vokoscreenNG
+cp com.github.vkohaupt.vokoscreenNG.yml com.github.vkohaupt.vokoscreenNG/com.github.vkohaupt.vokoscreenNG.yml
+cd com.github.vkohaupt.vokoscreenNG
+
+#flatpak-builder build-dir com.github.vkohaupt.vokoscreenNG.yml
+flatpak-builder --user --install --force-clean build-dir com.github.vkohaupt.vokoscreenNG.yml
+#flatpak run com.github.vkohaupt.vokoscreenNG
+
