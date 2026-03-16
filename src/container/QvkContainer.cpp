138196d372631d2f13b794e5016cda1dbd50e099
Volker Kohaupt <vkohaupt@freenet.de>
2026-03-16 15:43:11 +0100

    Flatpak: Add x264

--- src/container/QvkContainer.cpp
+++ src/container/QvkContainer.cpp
@@ -98,7 +98,8 @@ QvkContainer::QvkContainer(QObject *parent, bool isFlatpak ) : QObject(parent)
     Container *MKV = new Container( "matroskamux", "mkv" );
     MKV->add_VideoCodec( "openh264enc", "H.264" );
 #ifdef Q_OS_UNIX
-    if ( isFlatpak == false ) { MKV->add_VideoCodec( "x264enc", "x264"); }
+//    if ( isFlatpak == false ) { MKV->add_VideoCodec( "x264enc", "x264"); }
+    MKV->add_VideoCodec( "x264enc", "x264");
 #endif
     MKV->add_AudioCodec( "vorbisenc", "vorbis" );
     MKV->add_AudioCodec( "flacenc", "flac" );
@@ -108,7 +109,8 @@ QvkContainer::QvkContainer(QObject *parent, bool isFlatpak ) : QObject(parent)
     Container *MP4 = new Container( "mp4mux", "mp4" );
     MP4->add_VideoCodec( "openh264enc", "H.264" );
 #ifdef Q_OS_UNIX
-    if ( isFlatpak == false ) { MP4->add_VideoCodec( "x264enc", "x264" ); }
+//    if ( isFlatpak == false ) { MP4->add_VideoCodec( "x264enc", "x264" ); }
+    MP4->add_VideoCodec( "x264enc", "x264");
 #endif
     MP4->add_AudioCodec( "lamemp3enc", "mp3" );
     MP4->add_AudioCodec( "opusenc", "opus" );
