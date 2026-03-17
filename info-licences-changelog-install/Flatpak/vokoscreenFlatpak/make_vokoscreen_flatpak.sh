bc80fe57aaa0e8c5ec0baa221c3c231a5858e4f3
Volker Kohaupt <vkohaupt@freenet.de>
2026-03-17 11:22:53 +0100

    Flatpak

--- info-licences-changelog-install/Flatpak/vokoscreenFlatpak/make_vokoscreen_flatpak.sh
+++ info-licences-changelog-install/Flatpak/vokoscreenFlatpak/make_vokoscreen_flatpak.sh
@@ -1,14 +1,15 @@
 #/usr/bin/sh
 
+flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
 rm -rf com.github.vkohaupt.vokoscreenNG
 
-flatpak -y --user install org.kde.Sdk//6.6
-flatpak -y --user install org.kde.Platform//6.6
+flatpak -y --user install org.kde.Sdk//6.10
+flatpak -y --user install org.kde.Platform//6.10
 
 git clone https://github.com/flathub/com.github.vkohaupt.vokoscreenNG
 cp com.github.vkohaupt.vokoscreenNG.yml com.github.vkohaupt.vokoscreenNG/com.github.vkohaupt.vokoscreenNG.yml
 cd com.github.vkohaupt.vokoscreenNG
 
-#flatpak-builder build-dir com.github.vkohaupt.vokoscreenNG.yml
 flatpak-builder --user --install --force-clean build-dir com.github.vkohaupt.vokoscreenNG.yml
 
