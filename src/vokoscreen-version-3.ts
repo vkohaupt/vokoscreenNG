7dd3a6a26b665d3cef26576b27474809172cec9b
Volker Kohaupt <vkohaupt@freenet.de>
2026-05-27 17:09:37 +0200

    mirror is depraced

--- src/vokoscreen-version-3.ts
+++ src/vokoscreen-version-3.ts
@@ -51,13 +51,6 @@
     </message>
 </context>
 <context>
-    <name>QvkCameraSingle_wl</name>
-    <message>
-        <source>Form</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
     <name>QvkCameraSurface_wl</name>
     <message>
         <source>Camera</source>
