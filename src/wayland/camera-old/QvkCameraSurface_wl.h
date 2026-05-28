ea46796ba04166b7faf322d0f0154e358f3599aa
Volker Kohaupt <vkohaupt@freenet.de>
2026-05-28 19:46:10 +0200

    Wayland: Camera

---
+++ src/wayland/camera-old/QvkCameraSurface_wl.h
@@ -0,0 +1,97 @@
+/* vokoscreenNG - A desktop recorder
+ * Copyright (C) 2017-2024 Volker Kohaupt
+ *
+ * Author:
+ *      Volker Kohaupt <vkohaupt@volkoh.de>
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * --End_License--
+ */
+
+#ifndef QVKCAMERASURFACE_WL_H
+#define QVKCAMERASURFACE_WL_H
+
+#include <QObject>
+#include <QWidget>
+#include <QGuiApplication>
+#include <QPaintEvent>
+#include <QMouseEvent>
+#include <QEnterEvent>
+#include <QEvent>
+#include <QPixmap>
+#include <QRect>
+#include <QPoint>
+#include <QVideoFrame>
+#include <QToolButton>
+#include <QCheckBox>
+
+class QvkCameraSurface_wl : public QWidget
+{
+    Q_OBJECT
+public:
+    QvkCameraSurface_wl();
+    virtual ~QvkCameraSurface_wl();
+
+    void set_toolButtonRectangle(QToolButton *button);
+    void set_toolButtonElipse(QToolButton *button);
+    void set_toolButtonCircle(QToolButton *button);
+    QToolButton *toolButtonRectangle;
+    QToolButton *toolButtonElipse;
+    QToolButton *toolButtonCircle;
+
+    void set_toolButtonCameraMirrorVertical(QToolButton *toolButton);
+    void set_toolButtonCameraMirrorHorizontal(QToolButton *toolButton);
+    QToolButton *toolButtonCameraMirrorHorizontal;
+    QToolButton *toolButtonCameraMirrorVertical;
+
+
+    QPixmap pixmap;
+    QRect imageRect;
+
+    void set_newImageRect();
+    bool is_setNewImageRect = false;
+
+
+private:
+    int a = 0;
+    QPixmap cameraImage;
+    bool mousePressed = false;
+    QPoint pointDiv;
+    bool mouseHover = false;
+
+private slots:
+    void slot_workaroundForGnome_1();
+    void slot_workaroundForGnome_2();
+
+
+public slots:
+    void slot_setCameraImage(QVideoFrame videoFrame);
+
+
+signals:
+
+
+protected:
+    void paintEvent(QPaintEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    void mousePressEvent(QMouseEvent *event);
+    void mouseReleaseEvent(QMouseEvent *event);
+    void enterEvent(QEnterEvent *event);
+    void leaveEvent(QEvent *event );
+    void closeEvent(QCloseEvent *event);
+
+};
+
+#endif // QVKCAMERASURFACE_WL_H
---
+++ src/wayland/camera-old/QvkCameraWatcher_wl.cpp
@@ -0,0 +1,96 @@
+﻿/* vokoscreenNG - A desktop recorder
+ * Copyright (C) 2017-2024 Volker Kohaupt
+ *
+ * Author:
+ *      Volker Kohaupt <vkohaupt@volkoh.de>
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * --End_License--
+ */
+
+#include "QvkCameraWatcher_wl.h"
+#include "global.h"
+
+#include <QDebug>
+#include <QCameraDevice>
+#include <QMediaDevices>
+#include <QList>
+#include <QStringList>
+
+QvkCameraWatcher_wl::QvkCameraWatcher_wl()
+{
+}
+
+
+QvkCameraWatcher_wl::~QvkCameraWatcher_wl()
+{
+}
+
+void QvkCameraWatcher_wl::init()
+{
+    // object_id + ":::" + camera_name + ":::" + "added" or removed
+
+    QMediaDevices *mediaDevices = new QMediaDevices;
+    connect( mediaDevices, &QMediaDevices::videoInputsChanged, this, [=](){
+
+        QList<QCameraDevice> cameras = QMediaDevices::videoInputs();
+
+        // Camera wurde hinzugefügt
+        if(cameras.count() > stringListDevices.count()){
+            for(int i = 0; i < cameras.count(); i++){
+                QString id = cameras.at(i).id();
+                QString description = cameras.at(i).description();
+                QString id_and_description = id + ":::" + description;
+                if(!stringListDevices.contains(id_and_description)){
+                    stringListDevices.append(id_and_description);
+                    qDebug().noquote() << global::nameOutput << "[Camera] Added:" << description << "Device:" << id;
+                    emit signal_cameraChanged( id + ":::" + description + ":::" + "added" );
+                }
+            }
+            qDebug().noquote();
+            return;
+        }
+
+        // Camera wurde entfernt
+        // ID und description werden von devices in die StringListen tempID und tmpDescription transferiert
+        QStringList tmpList;
+        for(int i = 0; i < cameras.count(); i++){
+            tmpList.append(cameras.at(i).id() + ":::" + cameras.at(i).description());
+        }
+
+        int index = 0;
+        if(tmpList.count() < stringListDevices.count()){
+            for(int i = 0; i < stringListDevices.count(); i++){
+                QString id_and_description = stringListDevices.at(i);
+                if(!tmpList.contains(id_and_description)){
+                    // id_and_description wird nicht gefunden. In dem Fall ist es das gesuchte Gerät.
+                    QString id = id_and_description.section(":::", 0, 0);
+                    QString description = id_and_description.section(":::", 1, 1);
+                    qDebug().noquote() << global::nameOutput << "[Camera] Removed:" << description << "Device:" << id;
+                    emit signal_cameraChanged( id + ":::" + description + ":::" + "removed" );
+                    index = i;
+                    break;
+                }
+            }
+            if (!stringListDevices.empty()){
+                stringListDevices.removeAt(index);
+            }
+        }
+
+        qDebug().noquote();
+    });
+
+    emit mediaDevices->videoInputsChanged();
+}
