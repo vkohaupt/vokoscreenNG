590dbc5519251f36364d95386d21eba72d0ff4a9
Volker Kohaupt <vkohaupt@freenet.de>
2026-07-13 05:28:05 +0200

    X11 camera window

--- src/vokoscreen-version-3.ts
+++ src/vokoscreen-version-3.ts
@@ -651,10 +651,6 @@
         <source>Fullscreen</source>
         <translation type="unfinished"></translation>
     </message>
-    <message>
-        <source>...</source>
-        <translation type="unfinished"></translation>
-    </message>
 </context>
 <context>
     <name>formMainWindow_wl</name>
