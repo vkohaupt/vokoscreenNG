a7b5c38b272606e65baf1d2f7a7592f7bb4aa167
Volker Kohaupt <vkohaupt@freenet.de>
2026-07-19 12:43:48 +0200

    X11 camera window

--- src/cameraOne/QvkCameraOneOptions.cpp
+++ src/cameraOne/QvkCameraOneOptions.cpp
@@ -15,9 +15,12 @@ QvkCameraOneOptions::~QvkCameraOneOptions()
     delete ui;
 }
 
-
-void QvkCameraOneOptions::set_init(QString device)
+// Varibale m_device enthält zum Beispiel folgenden Inhalt
+// "/dev/video1:::UVC Camera (046d:0809):::added" or removed
+void QvkCameraOneOptions::set_init(QString m_device)
 {
+    QString device = "_" + m_device.section(":::", 0, 0);
+
     connect(ui->toolButtonCameraOneFramelessOnOff,
             &QToolButton::toggled,
             this, [=](bool value){
@@ -34,38 +37,38 @@ void QvkCameraOneOptions::set_init(QString device)
     setObjectName(m_objectName);
     ui->labelCameraOneOptions->setText(device.section(":::", 1, 1));
 
-    m_objectName = ui->toolButtonCameraOneViewRectangle->objectName() + "_" + device.section(":::", 0, 0);
+    m_objectName = ui->toolButtonCameraOneViewRectangle->objectName() + device;
     ui->toolButtonCameraOneViewRectangle->setObjectName(m_objectName);
 
-    m_objectName = ui->toolButtonCameraOneViewEllipse->objectName() + "_" + device.section(":::", 0, 0);
+    m_objectName = ui->toolButtonCameraOneViewEllipse->objectName() + device;
     ui->toolButtonCameraOneViewEllipse->setObjectName(m_objectName);
 
-    m_objectName = ui->toolButtonCameraOneViewCircle->objectName() + "_" + device.section(":::", 0, 0);
+    m_objectName = ui->toolButtonCameraOneViewCircle->objectName() + device;
     ui->toolButtonCameraOneViewCircle->setObjectName(m_objectName);
 
-    m_objectName = ui->lineEditCameraOneTitel->objectName() + "_" + device.section(":::", 0, 0);
+    m_objectName = ui->lineEditCameraOneTitel->objectName() + device;
     ui->lineEditCameraOneTitel->setObjectName(m_objectName);
 
-    m_objectName = ui->toolButtonCameraOneTitelReset->objectName() + "_" + device.section(":::", 0, 0);
+    m_objectName = ui->toolButtonCameraOneTitelReset->objectName() + device;
     ui->toolButtonCameraOneTitelReset->setObjectName(m_objectName);
 
-    m_objectName = ui->toolButtonCameraOneFramelessOnOff->objectName() + "_" + device.section(":::", 0, 0);
+    m_objectName = ui->toolButtonCameraOneFramelessOnOff->objectName() + device;
     ui->toolButtonCameraOneFramelessOnOff->setObjectName(m_objectName);
 
-    m_objectName = ui->toolButtonCameraOneMirrorVertical->objectName() + "_" + device.section(":::", 0, 0);
+    m_objectName = ui->toolButtonCameraOneMirrorVertical->objectName() + device;
     ui->toolButtonCameraOneMirrorVertical->setObjectName(m_objectName);
 
-    m_objectName = ui->toolButtonCameraOneMirrorHorizontal->objectName() + "_" + device.section(":::", 0, 0);
+    m_objectName = ui->toolButtonCameraOneMirrorHorizontal->objectName() + device;
     ui->toolButtonCameraOneMirrorHorizontal->setObjectName(m_objectName);
 
     QLabel *labelCameraWindowSize = new QLabel;
     ui->horizontalLayout_3->addWidget(labelCameraWindowSize);
-    labelCameraWindowSize->setObjectName("labelCameraOneWindowSize_" + device.section(":::", 0, 0));
+    labelCameraWindowSize->setObjectName("labelCameraOneWindowSize_" + device);
     labelCameraWindowSize->setText("---x---");
 
     QvkSpezialSlider *sliderCameraOneWindowSize = new QvkSpezialSlider(Qt::Horizontal);
     ui->horizontalLayout_2->insertWidget(0, sliderCameraOneWindowSize);
-    sliderCameraOneWindowSize->setObjectName("sliderCameraOneWindowSize_" + device.section(":::", 0, 0));
+    sliderCameraOneWindowSize->setObjectName("sliderCameraOneWindowSize_" + device);
     sliderCameraOneWindowSize->setMinimum(0);
     sliderCameraOneWindowSize->setMaximum(1);
     sliderCameraOneWindowSize->setValue(0);
@@ -76,7 +79,7 @@ void QvkCameraOneOptions::set_init(QString device)
 
     QvkSpezialSlider *sliderCameraOneWindowZoom = new QvkSpezialSlider(Qt::Horizontal);
     ui->horizontalLayout_5->insertWidget(0, sliderCameraOneWindowZoom);
-    sliderCameraOneWindowZoom->setObjectName("sliderCameraOneWindowZoom_" + device.section(":::", 0, 0));;
+    sliderCameraOneWindowZoom->setObjectName("sliderCameraOneWindowZoom_" + device);;
     sliderCameraOneWindowZoom->setMinimum(0);
     sliderCameraOneWindowZoom->setMaximum(1);
     sliderCameraOneWindowZoom->setValue(0);
