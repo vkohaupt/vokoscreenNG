97494944db10d158d37356bbe1901cc859afcf2f
Volker Kohaupt <vkohaupt@freenet.de>
2026-07-28 14:56:06 +0200

    X11 systray webcam

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -549,7 +549,7 @@ QvkMainWindow::QvkMainWindow(QWidget *parent) : QMainWindow(parent),
     vkSystray = new QvkSystray(ui);
     if (QSystemTrayIcon::isSystemTrayAvailable() == true){
         vkSystray->init();
-        connect(vkSystray,                 SIGNAL(signal_SystemtrayIsClose()),              this,      SLOT(close()));
+        connect(vkSystray, &QvkSystray::signal_SystemtrayIsClose, this, [=](){close();});
         connect(ui->checkBoxShowInSystray, SIGNAL(clicked(bool)),                           vkSystray, SLOT(setVisible(bool)));
         connect(vkGlobalShortcut,          SIGNAL(signal_shortcutSystray(QString,QString)), vkSystray, SLOT(slot_shortcutSystray(QString,QString)));
         ui->frameShowInSystrayAlternative->hide();
