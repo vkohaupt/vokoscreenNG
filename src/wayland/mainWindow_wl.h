4568cd3a3fae283208d01cb70d06a4d44ee70db2
Volker Kohaupt <vkohaupt@freenet.de>
2026-08-29 17:37:53 +0200

    Convert to mp4

--- src/wayland/mainWindow_wl.h
+++ src/wayland/mainWindow_wl.h
@@ -107,7 +107,6 @@ private:
 
     QString muxerVideoFilename = "";
     static GstBusSyncReply call_bus_message_convert_mp4(GstBus *bus, GstMessage *message, gpointer data);
-//    static GstBusSyncReply call_bus_set_state_to_null(GstBus *bus, GstMessage *message, gpointer m_pipeline);
     bool isFileOpenByAnyProcess(const QString &targetFilePath);
     GstElement *pipelineMP4;
 
