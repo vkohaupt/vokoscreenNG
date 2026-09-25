df9962cba9dae056460dba5612ad060eedab4ee6
Volker Kohaupt <vkohaupt@freenet.de>
2026-09-25 04:52:24 +0200

    Wayland: Add gif

--- src/wayland/convert/QvkConvert_mkv_gif_wl.cpp
+++ src/wayland/convert/QvkConvert_mkv_gif_wl.cpp
@@ -235,14 +235,14 @@ void QvkConvert_mkv_gif_wl::slot_convert_mkv_to_gif(QString filePath)
     QString VK_Pipeline;
     QString fileNameGIF = fileInfo.baseName() + ".gif";
     VK_Pipeline = "filesrc location=\"" + filePath + "\""
-            " ! matroskademux" +
-            " ! h264parse" +
-            " ! openh264dec" +
-            " ! queue max-size-buffers=0 max-size-time=0 max-size-bytes=104857600" +
-            " ! videoconvert" +
-            " ! gifenc speed=30 repeat=-1" +
+            " ! matroskademux name=demux demux.video_0"
+            " ! queue max-size-buffers=0 max-size-time=0 max-size-bytes=104857600"
+            " ! h264parse"
+            " ! openh264dec"
+            " ! queue max-size-buffers=0 max-size-time=0 max-size-bytes=104857600"
+            " ! videoconvert"
+            " ! gifenc speed=30 repeat=-1"
             " ! filesink location=\"" + path + "/" + fileNameGIF + "\"";
-
 
     qDebug().noquote() << global::nameOutput << "[Convert]" << VK_Pipeline;
 
